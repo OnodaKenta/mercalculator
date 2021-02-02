72073df93d340fbdd85d4b2f4db63327c31de5ea
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-02 23:28:19 +0900

    add:検索結果表示のため発送方法追加

--- app/main.rb
+++ app/main.rb
@@ -6,8 +6,34 @@ get "/" do
   erb :index
 end
 
+yuPacketPost = {
+  name: "ゆうパケットポスト",
+  price: 265,
+  lside: 32.7,
+  sside: 22.8,
+  thickness: 3,
+  weight: 2000,
+}
+
+nothing = {
+  name: "なし",
+  price: "なし",
+}
+
 post "/result" do
+
+  shippingmethod = []
+
+  if params[:longside].to_i <= yuPacketPost[:lside] && params[:shortside].to_i <= yuPacketPost[:sside] && params[:thickness].to_i <= yuPacketPost[:thickness] && params[:weight].to_i <= yuPacketPost[:weight]
+    shippingmethod.push(yuPacketPost)
+  end
+
+  if shippingmethod == []
+    shippingmethod.push(nothing)
+  end
+
   @title = "検索結果"
   @size = params
+  @shippingmethod = shippingmethod
   erb :result
 end
