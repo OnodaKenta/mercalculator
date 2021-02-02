65eaf85aeb038978f2ced389107227cdcac248ca
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-03 00:20:44 +0900

    update:発送方法をインスタンスで管理するためクラス作成

--- app/main.rb
+++ app/main.rb
@@ -6,34 +6,41 @@ get "/" do
   erb :index
 end
 
-yuPacketPost = {
-  name: "ゆうパケットポスト",
-  price: 265,
-  lside: 32.7,
-  sside: 22.8,
-  thickness: 3,
-  weight: 2000,
-}
+class ShippingMethod
+  attr_reader :name
+  attr_reader :price
+  attr_reader :lside
+  attr_reader :sside
+  attr_reader :thickness
+  attr_reader :weight
+  def initialize(name:, price:, lside:, sside:, thickness:, weight:)
+    self.name = name
+    self.price = price
+    self.lside = lside
+    self.sside = sside
+    self.thickness = thickness
+    self.weight = weight
+  end
+end
 
-nothing = {
-  name: "なし",
-  price: "なし",
-}
+yuPacketPost = ShippingMethod.new(name: "ゆうパケットポスト", price: 265, lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
+
+nothing = ShippingMethod.new(name: "なし", price: "-", lside: "-", sside: "-", thickness: "-", weight: "-")
 
 post "/result" do
 
-  shippingmethod = []
+  shippingmethods = []
 
-  if params[:longside].to_i <= yuPacketPost[:lside] && params[:shortside].to_i <= yuPacketPost[:sside] && params[:thickness].to_i <= yuPacketPost[:thickness] && params[:weight].to_i <= yuPacketPost[:weight]
-    shippingmethod.push(yuPacketPost)
+  if params[:longside].to_i <= yuPacketPost.lside && params[:shortside].to_i <= yuPacketPost.sside && params[:thickness].to_i <= yuPacketPost.thickness && params[:weight].to_i <= yuPacketPost.weight
+    shippingmethods.push(yuPacketPost)
   end
 
-  if shippingmethod == []
-    shippingmethod.push(nothing)
+  if shippingmethods == []
+    shippingmethods.push(nothing)
   end
 
   @title = "検索結果"
   @size = params
-  @shippingmethod = shippingmethod
+  @shippingmethods = shippingmethods
   erb :result
 end
