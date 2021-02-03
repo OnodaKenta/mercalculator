1454c9223699731579687cc60c861ce01592d849
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-03 20:02:42 +0900

    update:条件分岐繰り返しのためeachメソッド利用

--- app/main.rb
+++ app/main.rb
@@ -6,36 +6,43 @@ get "/" do
   erb :index
 end
 
-class ShippingMethod
-  attr_accessor :name
-  attr_accessor :price
-  attr_accessor :lside
-  attr_accessor :sside
-  attr_accessor :thickness
-  attr_accessor :weight
-  def initialize(name:, price:, lside:, sside:, thickness:, weight:)
-    self.name = name
-    self.price = price
-    self.lside = lside
-    self.sside = sside
-    self.thickness = thickness
-    self.weight = weight
-  end
-end
-
-yuPacketPost = ShippingMethod.new(name: "ゆうパケットポスト", price: 265, lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
-
-nothing = ShippingMethod.new(name: "なし", price: "-", lside: "-", sside: "-", thickness: "-", weight: "-")
-
 post "/result" do
+
+  $instances = []
+
+  class ShippingMethod
+    attr_accessor :name
+    attr_accessor :price
+    attr_accessor :lside
+    attr_accessor :sside
+    attr_accessor :thickness
+    attr_accessor :weight
+
+    def initialize(name:, price:, lside:, sside:, thickness:, weight:)
+      self.name = name
+      self.price = price
+      self.lside = lside
+      self.sside = sside
+      self.thickness = thickness
+      self.weight = weight
+      $instances.push(self)
+    end
+  end
+
+  yuPacketPost = ShippingMethod.new(name: "ゆうパケットポスト", price: 265, lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
+
+  takkyubinCompact = ShippingMethod.new(name: "宅急便コンパクト", price: 450, lside: 25, sside: 20, thickness: 5, weight: 99999)
 
   shippingmethods = []
 
-  if params[:longside].to_i <= yuPacketPost.lside && params[:shortside].to_i <= yuPacketPost.sside && params[:thickness].to_i <= yuPacketPost.thickness && params[:weight].to_i <= yuPacketPost.weight
-    shippingmethods.push(yuPacketPost)
+  $instances.each do |ins|
+    if params[:longside].to_i <= ins.lside && params[:shortside].to_i <= ins.sside && params[:thickness].to_i <= ins.thickness && params[:weight].to_i <= ins.weight
+      shippingmethods.push(ins)
+    end
   end
 
   if shippingmethods == []
+    nothing = ShippingMethod.new(name: "なし", price: "-", lside: "-", sside: "-", thickness: "-", weight: "-")
     shippingmethods.push(nothing)
   end
 
