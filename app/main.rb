35a8748ff35d8af156bc855ab4ee215b1d54d7dc
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-07 23:03:53 +0900

    update:可読性向上のためインスタンス作成場所変更

--- app/main.rb
+++ app/main.rb
@@ -7,15 +7,15 @@ get "/" do
   erb :index
 end
 
+$instances = []
+
+nekoPos = Lstw.new(name: "ネコポス", price: 175, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "長辺短辺下限あり", lside: 31.2, sside: 22.8, thickness: 3, weight: 1000)
+
+yuPacketPost = Lstw.new(name: "ゆうパケットポスト", price: 265, type: "ゆうゆうメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "専用BOX", lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
+
+clickPost = Lstw.new(name: "クリックポスト", price: 198, type: "日本郵便", anonymous: 0, tracking: 1, compensation: 0, note: "長辺短辺下限あり", lside: 34, sside: 25, thickness: 3, weight: 1000)
+
 post "/result" do
-
-  $instances = []
-
-  nekoPos = Lstw.new(name: "ネコポス", price: 175, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "長辺短辺下限あり", lside: 31.2, sside: 22.8, thickness: 3, weight: 1000)
-
-  yuPacketPost = Lstw.new(name: "ゆうパケットポスト", price: 265, type: "ゆうゆうメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "専用BOX", lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
-
-  clickPost = Lstw.new(name: "クリックポスト", price: 198, type: "日本郵便", anonymous: 0, tracking: 1, compensation: 0, note: "長辺短辺下限あり", lside: 34, sside: 25, thickness: 3, weight: 1000)
 
   shippingmethods = []
 
