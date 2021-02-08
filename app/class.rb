928eff95ddf3cc8208d4c1fc6c78eaefef02e3f2
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-09 00:21:36 +0900

    update:可読性向上のためインスタンス作成部分をclass.rbに移動

--- app/class.rb
+++ app/class.rb
@@ -42,3 +42,11 @@ class Lstw < Common
     $lstws.push(self)
   end
 end
+
+$lstws = []
+
+nekoPos = Lstw.new(name: "ネコポス", eng: "nekopos", price: 175, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "長辺短辺下限あり", lside: 31.2, sside: 22.8, thickness: 3, weight: 1000)
+
+yuPacketPost = Lstw.new(name: "ゆうパケットポスト", eng: "yupacketpost", price: 265, type: "ゆうゆうメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "専用BOX", lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
+
+clickPost = Lstw.new(name: "クリックポスト", eng: "clickpost", price: 198, type: "日本郵便", anonymous: 0, tracking: 1, compensation: 0, note: "長辺短辺下限あり", lside: 34, sside: 25, thickness: 3, weight: 1000)
