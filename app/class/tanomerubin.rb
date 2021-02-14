ac1e7439107ce581388f5008f9394f75ffcf28f5
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-15 00:26:58 +0900

    add:たのメル便インスタンス作成

--- app/class/tanomerubin.rb
+++ app/class/tanomerubin.rb
@@ -2,9 +2,8 @@ require "./app/class/common.rb"
 
 class Tanomerubin < Common
   attr_accessor :threesides
-  attr_accessor :weight
 
-  def initialize(name:, eng:, price:, type:, anonymous:, tracking:, compensation:, note:, threesides:, weight:)
+  def initialize(name:, eng:, price:, type:, anonymous:, tracking:, compensation:, note:, threesides:)
     @name = name
     @eng = eng
     @price = price
@@ -14,7 +13,10 @@ class Tanomerubin < Common
     @compensation = compensation
     @note = note
     @threesides = threesides
-    @weight = weight
-    $takkyubins.push(self)
+    $tanomerubins.push(self)
   end
 end
+
+$tanomerubins = []
+
+tanomerubin80 = Tanomerubin.new(name: "梱包・発送たのメル便80サイズ", eng: "tanomerubin80", price: 1700, type: "梱包・発送たのメル便", anonymous: 1, tracking: 1, compensation: 1, note: "梱包・集荷・発送をすべてプロにおまかせで大型商品を手間なく出品!", threesides: 80)
