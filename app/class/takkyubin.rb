3f27e48e12673121dd84b34d9f0ee9ce07955e64
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-15 19:12:10 +0900

    add:発送方法追加のため宅急便クラスにてインスタンス作成

--- app/class/takkyubin.rb
+++ app/class/takkyubin.rb
@@ -24,3 +24,11 @@ $takkyubins = []
 tbin60 = Takkyubin.new(name: "宅急便60サイズ", eng: "takkyubin60", price: 700, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 60, weight: 2000)
 
 tbin80 = Takkyubin.new(name: "宅急便80サイズ", eng: "takkyubin80", price: 800, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 80, weight: 5000)
+
+tbin100 = Takkyubin.new(name: "宅急便100サイズ", eng: "takkyubin100", price: 1000, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 100, weight: 10000)
+
+tbin120 = Takkyubin.new(name: "宅急便120サイズ", eng: "takkyubin120", price: 1100, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 120, weight: 15000)
+
+tbin140 = Takkyubin.new(name: "宅急便140サイズ", eng: "takkyubin140", price: 1300, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 140, weight: 20000)
+
+tbin160 = Takkyubin.new(name: "宅急便160サイズ", eng: "takkyubin160", price: 1600, type: "らくらくメルカリ便", anonymous: 1, tracking: 1, compensation: 1, note: "-", threesides: 160, weight: 25000)
