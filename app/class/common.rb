23b33d965b18e1095bcb0ed7a7e539f84d3ffc53
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-02-24 16:27:34 +0900

    update:送料合計を送料とBOX代に分割

--- app/class/common.rb
+++ app/class/common.rb
@@ -1,6 +1,8 @@
 class Common
   attr_accessor :name
   attr_accessor :url
+  attr_accessor :fee
+  attr_accessor :box
   attr_accessor :price
   attr_accessor :type
   attr_accessor :anonymous
@@ -13,10 +15,12 @@ class Common
   attr_accessor :threesides
   attr_accessor :weight
 
-  def initialize(name:, url:, price:, type:, anonymous:, tracking:, compensation:, note:)
+  def initialize(name:, url:, fee:, box:, type:, anonymous:, tracking:, compensation:, note:)
     @name = name
     @url = url
-    @price = price
+    @fee = fee
+    @box = box
+    @price = fee + box
     @type = type
     @anonymous = anonymous
     @tracking = tracking
@@ -25,24 +29,24 @@ class Common
   end
 end
 
-$yuPacket = Common.new(name: "ゆうパケット", url: "/method/yuPacket", price: 200, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "-")
+$yuPacket = Common.new(name: "ゆうパケット", url: "/method/yuPacket", fee: 200, box: 0, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "-")
 $yuPacket.lside = 34
 $yuPacket.thickness = 3
 $yuPacket.threesides = 60
 $yuPacket.weight = 1000
 
-$letterPackPlus = Common.new(name: "レターパックプラス", url: "/method/letterPackPlus", price: 520, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "専用封筒入れば厚さ3cm以上可")
+$letterPackPlus = Common.new(name: "レターパックプラス", url: "/method/letterPackPlus", fee: 520, box: 0, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "専用封筒入れば厚さ3cm以上可")
 $letterPackPlus.lside = 34
 $letterPackPlus.sside = 24.8
 $letterPackPlus.weight = 4000
 
-$takkyubinCompactThin = Common.new(name: "宅急便コンパクト薄型BOX", url: "/method/takkyubinCompactThin", price: 450, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "厚さ注意!専用薄型BOX(70円)")
+$takkyubinCompactThin = Common.new(name: "宅急便コンパクト薄型BOX", url: "/method/takkyubinCompactThin", fee: 380, box: 70, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "厚さ注意!専用薄型BOX(70円)")
 $takkyubinCompactThin.lside = 34
 $takkyubinCompactThin.sside = 24.8
 
-$takkyubinCompact = Common.new(name: "宅急便コンパクト", url: "/method/takkyubinCompact", price: 450, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX(70円)")
+$takkyubinCompact = Common.new(name: "宅急便コンパクト", url: "/method/takkyubinCompact", fee: 380, box: 70, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX(70円)")
 $takkyubinCompact.lside = 25
 $takkyubinCompact.sside = 20
 $takkyubinCompact.thickness = 5
 
-$nothing = Common.new(name: "なし", url: "none", price: 0, type: "-", anonymous: "×", tracking: "×", compensation: "×", note: "-")
+$nothing = Common.new(name: "なし", url: "none", fee: 0, box: 0, type: "-", anonymous: "×", tracking: "×", compensation: "×", note: "-")
--- app/class/lstw.rb
+++ app/class/lstw.rb
@@ -1,10 +1,12 @@
 require "./app/class/common.rb"
 
 class Lstw < Common
-  def initialize(name:, url:, price:, type:, anonymous:, tracking:, compensation:, note:, lside:, sside:, thickness:, weight:)
+  def initialize(name:, url:, fee:, box:, type:, anonymous:, tracking:, compensation:, note:, lside:, sside:, thickness:, weight:)
     @name = name
     @url = url
-    @price = price
+    @fee = fee
+    @box = box
+    @price = fee + box
     @type = type
     @anonymous = anonymous
     @tracking = tracking
@@ -20,9 +22,9 @@ end
 
 $lstws = []
 
-nekoPos = Lstw.new(name: "ネコポス", url: "/method/nekoPos", price: 175, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "長辺短辺下限あり", lside: 31.2, sside: 22.8, thickness: 3, weight: 1000)
-yuPacketPost = Lstw.new(name: "ゆうパケットポスト", url: "/method/yuPacketPost", price: 265, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX", lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
-clickPost = Lstw.new(name: "クリックポスト", url: "/method/clickPost", price: 198, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "長辺短辺下限あり", lside: 34, sside: 25, thickness: 3, weight: 1000)
-smartLetter = Lstw.new(name: "スマートレター", url: "/method/smartLetter", price: 180, type: "日本郵便", anonymous: "×", tracking: "×", compensation: "×", note: "専用封筒A5サイズ", lside: 25, sside: 17, thickness: 2, weight: 1000)
-letterPackLight = Lstw.new(name: "レターパックライト", url: "/method/letterPackLight", price: 370, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "専用封筒A4サイズ", lside: 34, sside: 24.8, thickness: 3, weight: 4000)
-yuPacketPlus = Lstw.new(name: "ゆうパケットプラス", url: "/method/yuPacketPlus", price: 440, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX", lside: 24, sside: 17, thickness: 7, weight: 2000)
+nekoPos = Lstw.new(name: "ネコポス", url: "/method/nekoPos", fee: 175, box: 0, type: "らくらくメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "長辺短辺下限あり", lside: 31.2, sside: 22.8, thickness: 3, weight: 1000)
+yuPacketPost = Lstw.new(name: "ゆうパケットポスト", url: "/method/yuPacketPost", fee: 200, box: 65, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX", lside: 32.7, sside: 22.8, thickness: 3, weight: 2000)
+clickPost = Lstw.new(name: "クリックポスト", url: "/method/clickPost", fee: 198, box: 0, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "長辺短辺下限あり", lside: 34, sside: 25, thickness: 3, weight: 1000)
+smartLetter = Lstw.new(name: "スマートレター", url: "/method/smartLetter", fee: 180, box: 0, type: "日本郵便", anonymous: "×", tracking: "×", compensation: "×", note: "専用封筒A5サイズ", lside: 25, sside: 17, thickness: 2, weight: 1000)
+letterPackLight = Lstw.new(name: "レターパックライト", url: "/method/letterPackLight", fee: 370, box: 0, type: "日本郵便", anonymous: "×", tracking: "○", compensation: "×", note: "専用封筒A4サイズ", lside: 34, sside: 24.8, thickness: 3, weight: 4000)
+yuPacketPlus = Lstw.new(name: "ゆうパケットプラス", url: "/method/yuPacketPlus", fee: 375, box: 65, type: "ゆうゆうメルカリ便", anonymous: "○", tracking: "○", compensation: "○", note: "専用BOX", lside: 24, sside: 17, thickness: 7, weight: 2000)
