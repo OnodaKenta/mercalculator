790be408c3b94ff223db1e1222001582c6f9be03
OnodaKenta <kenta.sfpht0111.onoda@gmail.com>
2021-03-02 21:12:09 +0900

    add:Commonクラステスト項目追加

--- spec/common_spec.rb
+++ spec/common_spec.rb
@@ -4,11 +4,29 @@ RSpec.describe $yuPacket do
   it "$yuPacketのnameは正しいか" do
     expect($yuPacket.name).to eq "ゆうパケット"
   end
+  it "$yuPacketのurlは正しいか" do
+    expect($yuPacket.url).to eq "/method/yuPacket"
+  end
   it "$yuPacketのfeeは正しいか" do
     expect($yuPacket.fee).to eq 200
   end
   it "$yuPacketのboxは正しいか" do
     expect($yuPacket.box).to eq 0
+  end
+  it "$yuPacketのpriceは正しいか" do
+    expect($yuPacket.price).to eq 200
+  end
+  it "$yuPacketのtypeは正しいか" do
+    expect($yuPacket.type).to eq "ゆうゆうメルカリ便"
+  end
+  it "$yuPacketのanonymousは正しいか" do
+    expect($yuPacket.anonymous).to eq "○"
+  end
+  it "$yuPacketのtrackingは正しいか" do
+    expect($yuPacket.tracking).to eq "○"
+  end
+  it "$yuPacketのcompensationは正しいか" do
+    expect($yuPacket.compensation).to eq "○"
   end
   it "$yuPacketのlsideは正しいか" do
     expect($yuPacket.lside).to eq 34
@@ -31,11 +49,29 @@ RSpec.describe $letterPackPlus do
   it "$letterPackPlusのnameは正しいか" do
     expect($letterPackPlus.name).to eq "レターパックプラス"
   end
+  it "$letterPackPlusのurlは正しいか" do
+    expect($letterPackPlus.url).to eq "/method/letterPackPlus"
+  end
   it "$letterPackPlusのfeeは正しいか" do
     expect($letterPackPlus.fee).to eq 520
   end
   it "$letterPackPlusのboxは正しいか" do
     expect($letterPackPlus.box).to eq 0
+  end
+  it "$letterPackPlusのpriceは正しいか" do
+    expect($letterPackPlus.price).to eq 520
+  end
+  it "$letterPackPlusのtypeは正しいか" do
+    expect($letterPackPlus.type).to eq "日本郵便"
+  end
+  it "$letterPackPlusのanonymousは正しいか" do
+    expect($letterPackPlus.anonymous).to eq "×"
+  end
+  it "$letterPackPlusのtrackingは正しいか" do
+    expect($letterPackPlus.tracking).to eq "○"
+  end
+  it "$letterPackPlusのcompensationは正しいか" do
+    expect($letterPackPlus.compensation).to eq "×"
   end
   it "$letterPackPlusのlsideは正しいか" do
     expect($letterPackPlus.lside).to eq 34
@@ -58,11 +94,29 @@ RSpec.describe $takkyubinCompact do
   it "$takkyubinCompactのnameは正しいか" do
     expect($takkyubinCompact.name).to eq "宅急便コンパクト"
   end
+  it "$takkyubinCompactのurlは正しいか" do
+    expect($takkyubinCompact.url).to eq "/method/takkyubinCompact"
+  end
   it "$takkyubinCompactのfeeは正しいか" do
     expect($takkyubinCompact.fee).to eq 380
   end
   it "$takkyubinCompactのboxは正しいか" do
     expect($takkyubinCompact.box).to eq 70
+  end
+  it "$takkyubinCompactのpriceは正しいか" do
+    expect($takkyubinCompact.price).to eq 450
+  end
+  it "$takkyubinCompactのtypeは正しいか" do
+    expect($takkyubinCompact.type).to eq "らくらくメルカリ便"
+  end
+  it "$takkyubinCompactのanonymousは正しいか" do
+    expect($takkyubinCompact.anonymous).to eq "○"
+  end
+  it "$takkyubinCompactのtrackingは正しいか" do
+    expect($takkyubinCompact.tracking).to eq "○"
+  end
+  it "$takkyubinCompactのcompensationは正しいか" do
+    expect($takkyubinCompact.compensation).to eq "○"
   end
   it "$takkyubinCompactのlsideは正しいか" do
     expect($takkyubinCompact.lside).to eq 25
@@ -85,11 +139,29 @@ RSpec.describe $takkyubinCompactThin do
   it "$takkyubinCompactThinのnameは正しいか" do
     expect($takkyubinCompactThin.name).to eq "宅急便コンパクト薄型BOX"
   end
+  it "$takkyubinCompactThinのurlは正しいか" do
+    expect($takkyubinCompactThin.url).to eq "/method/takkyubinCompact"
+  end
   it "$takkyubinCompactThinのfeeは正しいか" do
     expect($takkyubinCompactThin.fee).to eq 380
   end
   it "$takkyubinCompactThinのboxは正しいか" do
     expect($takkyubinCompactThin.box).to eq 70
+  end
+  it "$takkyubinCompactThinのpriceは正しいか" do
+    expect($takkyubinCompactThin.price).to eq 450
+  end
+  it "$takkyubinCompactThinのtypeは正しいか" do
+    expect($takkyubinCompactThin.type).to eq "らくらくメルカリ便"
+  end
+  it "$takkyubinCompactThinのanonymousは正しいか" do
+    expect($takkyubinCompactThin.anonymous).to eq "○"
+  end
+  it "$takkyubinCompactThinのtrackingは正しいか" do
+    expect($takkyubinCompactThin.tracking).to eq "○"
+  end
+  it "$takkyubinCompactThinのcompensationは正しいか" do
+    expect($takkyubinCompactThin.compensation).to eq "○"
   end
   it "$takkyubinCompactThinのlsideは正しいか" do
     expect($takkyubinCompactThin.lside).to eq 34
